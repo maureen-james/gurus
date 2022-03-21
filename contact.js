db600a52b0fcd39f9664c8b331325b284955aad7
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-22 00:25:44 +0300

    Add Js function to validate contact form

---
+++ contact.js
@@ -0,0 +1,7 @@
+// get in touch form
+function send() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var message = document.getElementById("message").value;
+};
